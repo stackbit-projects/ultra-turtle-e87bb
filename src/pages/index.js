c67b61a68be92f134a928d36a8afe56a72fe3e79
Stackbit Code Editor <projects@stackbit.com>
2021-06-07 21:02:03 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,28 +23,6 @@ const IndexPage = () => (
       </div>
     </section>
 
-    <section id="wrapper">
-      <section id="one" className="wrapper spotlight style1">
-        <div className="inner">
-          <a href="/#" className="image">
-            <img src={pic1} alt="" />
-          </a>
-          <div className="content">
-            <h2 className="major">Magna arcu feugiat</h2>
-            <p>
-              Lorem ipsum dolor sit amet, etiam lorem adipiscing elit. Cras
-              turpis ante, nullam sit amet turpis non, sollicitudin posuere
-              urna. Mauris id tellus arcu. Nunc vehicula id nulla dignissim
-              dapibus. Nullam ultrices, neque et faucibus viverra, ex nulla
-              cursus.
-            </p>
-            <a href="/#" className="special">
-              Learn more
-            </a>
-          </div>
-        </div>
-      </section>
-
 
 
     </section>
